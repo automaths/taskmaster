af40542eb321aa3ebb674fdf59ab6e248d01c531
automaths <nicolassartral@gmail.com>
2023-10-03 22:02:12 +0200

    yaml file style parsing

--- Config.hpp
+++ Config.hpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <exception>
 #include <unordered_map>
+#include <vector>
 #include "utils.hpp"
 
 class Config {
@@ -12,26 +13,38 @@ public:
         std::string l_fileContent = file2string(p_filename);
         std::string l_line;
         std::string l_key;
+        std::string l_mapkey;
         std::string l_value;
         std::stringstream l_stream(l_fileContent);
         while (std::getline(l_stream, l_line)) {
-            if (l_line[0] == '#') {
+            if (l_line.empty()) {
                 continue;
             }
-            std::stringstream l_lineStream(l_line);
-            std::getline(l_lineStream, l_key, ':');
-            std::getline(l_lineStream, l_value);
-            m_config[l_key] = l_value;
+            else if (l_line.find_first_not_of(' ') == 0) {
+                l_key = l_line.substr(0, l_line.find_last_not_of(" :") + 1);
+                m_config.push_back(std::make_pair(l_key, std::unordered_map<std::string, std::string>()));
+            }
+            else if (l_line.find_first_not_of(' ') == 2) {
+                l_mapkey = l_line.substr(2, l_line.find_first_of(':'));
+                l_value = l_line.substr(l_line.find_first_of(':') + 1, l_line.find_last_not_of(" ") + 1);
+                m_config.back().second[l_mapkey] = l_value;
+            }
+            else {
+                throw std::runtime_error("Invalid config file format.");
+            }
         }
     }
 
     void printConfig() {
-        for (auto& l_pair : m_config) {
-            std::cout << l_pair.first << " = " << l_pair.second << std::endl;
+        for (auto& l_section : m_config) {
+            std::cout << l_section.first << std::endl;
+            for (auto& l_keyValue : l_section.second) {
+                std::cout << "    " << l_keyValue.first << " : " << l_keyValue.second << std::endl;
+            }
         }
     }
 
 private:
 
-    std::unordered_map<std::string, std::string> m_config;
+    std::vector<std::pair<std::string, std::unordered_map<std::string, std::string>>> m_config;
 };
